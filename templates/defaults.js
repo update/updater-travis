559a6ca50d6cec0eb0f62274436a0d5822913686
jonschlinkert <jon.schlinkert@sellside.com>
2017-07-11 06:34:50 -0400

    update templates

--- templates/defaults.js
+++ templates/defaults.js
@@ -4,12 +4,5 @@ module.exports = {
   sudo: false,
   os: ['linux', 'osx'],
   language: 'node_js',
-  node_js: [ 'node', '7', '6', '5', '4', '0.12', '0.10' ],
-  matrix: {
-    allow_failures: [
-      {node_js: '4'},
-      {node_js: '0.12'},
-      {node_js: '0.10'}
-    ]
-  }
+  node_js: [ 'node', '8', '7', '6', '5', '4', '0.12', '0.10' ]
 };
